b00ac3a51aa71dadce742198abb5c7915a01a100
inq39 <61730491+inq39@users.noreply.github.com>
2021-03-17 22:30:55 +0100

    MenuManager,

---
+++ Assets/Operation_Broken_Wing/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Operation_Broken_Wing.UI;
+
+namespace Operation_Broken_Wing.Manager
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Menu mainMenu;
+        [SerializeField]
+        private Menu settingsMenu;
+        [SerializeField]
+        private Transform _menuParent;
+        private Stack<Menu> _menuStack = new Stack<Menu>();
+
+        private void Awake()
+        {
+            InitializeMenus();
+        }
+
+        private void InitializeMenus()
+        {
+            if (_menuParent == null)
+                Debug.Log("Menu Folder not set.");
+
+            Menu[] menuPrefabs = new[]{mainMenu, settingsMenu};
+            foreach (Menu prefab in menuPrefabs)
+            {
+                if (prefab != null)
+                {
+                    Menu newMenu = Instantiate(prefab, _menuParent);
+                    if (prefab != mainMenu)
+                    {
+                        newMenu.gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        OpenMenu(newMenu);
+                    }
+                }
+            }
+        }
+
+        public void OpenMenu(Menu menuInstance)
+        {
+            if (menuInstance == null)
+            {
+                Debug.LogError("NO MenuInstance!");
+                return;
+            }
+
+            if (_menuStack.Count > 0)
+            {
+                foreach (Menu menu in _menuStack)
+                {
+                    menu.gameObject.SetActive(false);
+                }
+            }
+            menuInstance.gameObject.SetActive(true);
+            _menuStack.Push(menuInstance);
+
+        }
+
+        public void CloseMenu()
+        {
+            if (_menuStack.Count == 0)
+            {
+                Debug.LogError("MenuManager: No Menu in stack!");
+                return;
+            }
+            var topMenu = _menuStack.Pop();
+            topMenu.gameObject.SetActive(false);
+
+            if (_menuStack.Count > 0)
+            {
+                var nextMenu = _menuStack.Peek();
+                nextMenu.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Operation_Broken_Wing/Scripts/UI/Menu.cs
@@ -0,0 +1,29 @@
+﻿using Operation_Broken_Wing.Manager;
+using UnityEngine;
+
+namespace Operation_Broken_Wing.UI
+{
+    [RequireComponent(typeof(Canvas))]
+    public class Menu : MonoBehaviour
+    {
+        public void OnBackButton()
+        {
+            
+        }
+
+        public void OnPlayButton()
+        {
+            LevelManager.Instance.LoadNextLevel();
+        }
+
+        public void OnOptionsButton()
+        {
+            
+        }
+
+        public void OnQuitButton()
+        {
+            LevelManager.Instance.QuitGame();
+        }
+    }
+}
